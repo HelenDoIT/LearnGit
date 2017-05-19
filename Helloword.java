3493401622e090c020ab3556f64c6c20b3fb5a8a
Lenka <1024303455@qq.com>
2017-05-19 10:30:35 +0800

    add Starting Coding

--- Helloword.java
+++ Helloword.java
@@ -3,7 +3,7 @@ public class Helloword{
 public static void main (String[]agrs){
 System.out.println("Day one");
 System.out.print("Homework");
-System.out.println("Just Do IT");
+System.out.println("Just Do IT and Starting Coding");
 }
 
 }
